638606b1d67a8c2418129a046c2b43af182fb303
Omar <ramo_24@outlook.com>
2021-01-14 16:08:05 +0200

    Equation to matrix

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import eq_mat as matrix
 
 numberOfVariables = 3
-eq = ["5x+3y+6z=5","6x+5y+1z=6","x+y+z=1"]
+
+#if a variable does not exists in equation it should bew multiplied by zero
+#   x+2y=0 => x+2y+0z=0
+
+eq = ["-6x+5y+1z=6","x+y+z=1","x+2y+0z=0"]
 Matrix = matrix.equationToMatrix(numberOfVariables,eq)
 print(Matrix)
